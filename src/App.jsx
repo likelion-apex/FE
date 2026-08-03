390f4fe833437d410257648f73a8c162db87f6a6
tjdud9329 <tjdud9329@gmail.com>
2026-08-04 02:23:53 +0900

    [Environment] node 및 tailwindcss 설정

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-lg text-red-400"> 서영 </div>;
+}
+
+export default App;
