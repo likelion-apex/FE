3264c7985ff6c097bf45f0fa8f64e973b4a2ea0d
ifapqle <vousmevoyez889@gmail.com>
2026-08-04 22:01:25 +0900

    [Feat] 카카오 로그인 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Onboarding from "./pages/Onboarding";
+import KakaoCallback from "./pages/KakaoCallback";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="text-lg text-red-400"> 서영 </div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
