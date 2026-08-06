69ea3e1893aca6f8d257378fcfbe92812c5f3d9f
tjdud9329 <tjdud9329@gmail.com>
2026-08-06 15:08:35 +0900

    [!HOTFIX] App.css 리턴문 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-10 flex justify-center items-center">
       {/* 아이폰 17 컨테이너*/}
-      <main className="w-[402px] h-[874px] bg-white text-black overflow-y-auto relative no-scrollbar"></main>
+      <main className="w-[402px] h-[874px] bg-white text-black overflow-y-auto relative no-scrollbar">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Onboarding />} />
+            <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Onboarding />} />
-        <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
-      </Routes>
-    </BrowserRouter>
   );
 }
 
