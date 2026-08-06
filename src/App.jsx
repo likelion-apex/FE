3b9eace47cef42f891c3d15b21c50d246d908b42
tjdud9329 <tjdud9329@gmail.com>
2026-08-06 14:07:21 +0900

    [Environment] 초기 레이아웃 및 글꼴/색상 설정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="text-lg text-red-400"> 서영 </div>;
+  return (
+    <div className="min-h-screen bg-gray-10 flex justify-center items-center">
+      {/* 아이폰 17 컨테이너*/}
+      <main className="w-[402px] h-[874px] bg-white text-black overflow-y-auto relative no-scrollbar"></main>
+    </div>
+  );
 }
 
 export default App;
