4eb170989e020d525a3fe2e8558d02af1238b8aa
tjdud9329 <tjdud9329@gmail.com>
2026-08-06 16:19:14 +0900

    [Mod] 카카오 로그인 아이콘 수정

--- src/components/KakaoLoginButton.jsx
+++ src/components/KakaoLoginButton.jsx
@@ -1,3 +1,5 @@
+import KakaoIcon from "../assets/kakaoIcon.png";
+
 const KakaoLoginButton = () => {
   const handleKakaoLogin = () => {
     if (!window.Kakao) {
@@ -14,7 +16,19 @@ const KakaoLoginButton = () => {
     });
   };
 
-  return <button onClick={handleKakaoLogin}>카카오 로그인</button>;
+  return (
+    <button
+      onClick={handleKakaoLogin}
+      className="h-[68px] w-[360px] flex justify-center items-center bg-[#FFE812] rounded-[10px] cursor-pointer"
+    >
+      <img
+        src={KakaoIcon}
+        alt="카카오 로그인"
+        className="w-6 h-6 mr-[60px] ml-[20px]"
+      />
+      <p className="text-black font-bold text-[18px]">카카오로 계속하기</p>
+    </button>
+  );
 };
 
 export default KakaoLoginButton;
