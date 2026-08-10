89bc9c5a9fcc5ef136a36414669e4efc543a411a
tjdud9329 <tjdud9329@gmail.com>
2026-08-10 20:16:31 +0900

    [Feat] 내피부 맞춤 최적화 페이지 및 액션 모달 구현

---
+++ src/components/Analysis/FinishModal.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import Button from "./Button";
+
+const FinishModal = () => {
+  useEffect(() => {
+    const scrollBox = document.getElementById("main-scroll-box");
+
+    if (scrollBox) {
+      // 모달이 열리면 이 박스의 스크롤을 막음
+      scrollBox.style.overflow = "hidden";
+    }
+
+    return () => {
+      if (scrollBox) {
+        // 모달이 닫히면 다시 스크롤되도록 풀어줌
+        scrollBox.style.overflow = "auto";
+      }
+    };
+  }, []);
+  return (
+    <div className="fixed inset-y-0 left-1/2 z-[999] flex w-full max-w-[402px] -translate-x-1/2 flex-col justify-end bg-black/60 no-scrollbar">
+      <div
+        className="flex h-[35vh] w-full flex-col rounded-t-[24px] bg-white px-[24px]"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex flex-col gap-3 text-blue-50 items-center mt-6 mb-7 text-[14px]">
+          <p className="font-bold">내 화장대 기반 맞춤 최적화 완료</p>
+          <div className="flex flex-col gap-2">
+            <p className="text-[20px] font-semibold text-black">
+              이 안전한 루틴으로 오늘 케어할까요?
+            </p>
+            <p className="text-[14px]  text-gray-80">
+              저장 시 홈 화면의 '오늘의 데일리 루틴'에 바로 추가돼요.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 pb-[20px]">
+          <Button
+            item={"이 루틴으로 오늘 시작하기"}
+            bgColor={"blue-50"}
+            textColor={"white"}
+            borderColor={"border-blue-50"}
+          />
+          <Button
+            item={"나의 루틴 보관함에 저장"}
+            bgColor={"white"}
+            textColor={"gray-60"}
+            borderColor={"border-gray-20"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FinishModal;
--- src/pages/RoutineAnalysis/OptimizedRoutine.jsx
+++ src/pages/RoutineAnalysis/OptimizedRoutine.jsx
@@ -1,5 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import RoutineAccordionItem from "../../components/Analysis/RoutineAccordionItem";
+import FinishModal from "../../components/Analysis/FinishModal";
+import {
+  USER_NAME,
+  MATCHING_REPORT_DATA,
+  ROUTINE_STEPS,
+} from "../../mocks/mockData";
+
 
 const OptimizedRoutine = () => {
   const { setNavProps } = useOutletContext();
@@ -19,7 +27,87 @@ const OptimizedRoutine = () => {
       });
     };
   }, [setNavProps]);
-  return <div> 최적화 페이지</div>;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="relative flex h-full flex-col px-[20px]">
+      <div className="flex-1 overflow-y-auto pb-[100px] pt-6">
+        <div className="mb-6 flex flex-col gap-2">
+          <h1 className="break-keep text-[22px] font-semibold  leading-snug text-black">
+            {USER_NAME}님의 인벤토리 제품으로
+            <br />
+            안전하게 루틴을 재구성했어요
+          </h1>
+          <p className="text-[13px] text-gray-60">
+            자극적인 성분은 빼고, 역할이 같은 제품으로!
+          </p>
+        </div>
+        {/* 이부분 컴포넌트로 빼는게 좋을것같음!!! */}
+        <div className="mb-8">
+          <div className="flex w-full flex-col gap-2 rounded-[24px] bg-gray-05 px-6 py-4">
+            <div className="flex">
+              <span className="rounded-md bg-gray-10 px-[8px] py-[4px] text-[12px] font-bold text-gray-60">
+                AI 매칭 리포트
+              </span>
+            </div>
+
+            <div className="flex flex-col gap-[10px] border-b border-gray-20 pb-4 pt-2 text-[13px]">
+              <div className="flex items-center justify-between">
+                <span className="text-gray-60 font-normal">
+                  그대로 사용 가능한 제품
+                </span>
+                <span className="font-bold text-black">
+                  {MATCHING_REPORT_DATA.keepCount}개 완료
+                </span>
+              </div>
+              <div className="flex items-center justify-between">
+                <span className="text-gray-60 font-normal">
+                  AI가 안전하게 대체한 제품
+                </span>
+                <span className="font-bold text-blue-50">
+                  {MATCHING_REPORT_DATA.replacedCount}개
+                  {MATCHING_REPORT_DATA.replacedDetail}
+                </span>
+              </div>
+            </div>
+
+            {/* 최하단: 상세 설명 */}
+            <p className="text-[13px] leading-[22px] text-gray-600">
+              {MATCHING_REPORT_DATA.description}
+            </p>
+          </div>
+        </div>
+
+        {/* 아코디언 리스트 섹션 */}
+        <div>
+          <h2 className="mb-6 text-[16px] font-semibold text-gray-900">
+            오늘 밤을 위한 안전한 4단계 루틴
+          </h2>
+          <div className="flex flex-col gap-2">
+            {ROUTINE_STEPS.map((step) => (
+              <RoutineAccordionItem key={step.id} step={step} />
+            ))}
+          </div>
+        </div>
+      </div>
+
+      {/* 5. 하단 고정 버튼 영역 */}
+      <div className="flex flex-col gap-2 items-center justify-center w-full mb-5 mt-3">
+        <button
+          type="button"
+          className="flex w-full h-[56px] items-center justify-center rounded-[10px] bg-blue-50 px-10 py-2 text-[18px] font-medium text-white cursor-pointer"
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          이 안전한 루틴으로 오늘 케어하기
+        </button>
+      </div>
+
+      {isModalOpen && <FinishModal />}
+    </div>
+  );
 };
 
 export default OptimizedRoutine;
