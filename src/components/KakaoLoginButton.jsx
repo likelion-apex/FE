bf1572dfb8c1788839bdc010ded6afed9a41fe8b
ifapqle <vousmevoyez889@gmail.com>
2026-08-15 15:12:44 +0900

    [Mod] 온보딩 UI 수정

--- src/components/KakaoLoginButton.jsx
+++ src/components/KakaoLoginButton.jsx
@@ -16,11 +16,11 @@ const KakaoLoginButton = () => {
       className="flex h-[68px] w-full cursor-pointer items-center justify-center gap-[50px] rounded-lg bg-white px-5 py-4"
     >
       <img src={KakaoCircle} alt="" className="size-[35px] shrink-0" />
-      <p className="text-[18px] leading-[30px]  text-blue-60">
+      <p className="text-[18px] leading-[30px] font-semibold text-blue-60">
         카카오로 시작하기
       </p>
       {/* 텍스트를 버튼 가운데로 맞추기 위한 여백 (Figma의 opacity 0 아이콘) */}
-      <span aria-hidden className="size-[35px] font-semibold shrink-0 " />
+      <span aria-hidden className="size-[35px] shrink-0" />
     </button>
   );
 };
